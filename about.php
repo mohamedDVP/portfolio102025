7d9310abce3fba005a99d2516137a07fe254e9f9
mohamedDVP <mohamed.mak@laposte.net>
2026-02-03 21:16:29 +0100

    working on about page

--- about.php
+++ about.php
@@ -1,9 +1,36 @@
 <?php include 'includes/header.php'; ?>
+<?php include 'includes/data.php'; ?>
 <main>
     <div class="container-fluid">
         <h1>À propos de moi</h1>
-        <p>Page en construction...</p>
-        <p>Ici, vous pourrez parler de votre parcours et de vos compétences.</p>
+        
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-8">
+                <div class="card p-4">
+                    <h2>Bonjour, je suis <?php echo $infos_perso['nom']; ?></h2>
+                    <h3 class="text-muted"><?php echo $infos_perso['poste']; ?></h3>
+                    <hr>
+                    <p>
+                        Passionné par le développement web, j'ai commencé mon apprentissage en créant des sites statiques avant de plonger
+                        dans l'univers dynamique du PHP.
+                    </p>
+                    <p>
+                        Mon objectif est de créer des applications web performantes et utiles.
+                    </p>
+                    
+                    <h4 class="mt-4">Mes Compétences</h4>
+                    <ul>
+                        <li>HTML5 / CSS3</li>
+                        <li>PHP / MySQL (En cours d'apprentissage)</li>
+                        <li>Git / GitHub</li>
+                    </ul>
+
+                    <div class="mt-4 text-center">
+                        <a href="contact.php" class="button-primary">Me contacter</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 <?php include 'includes/footer.php'; ?>
