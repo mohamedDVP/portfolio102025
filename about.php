b06874930e68c345cd4f9286f96fd537386ed73a
mohamedDVP <mohamed.mak@laposte.net>
2026-02-03 21:00:36 +0100

    add about contact and portfolio pages

--- about.php
+++ about.php
@@ -1,9 +1,9 @@
 <?php include 'includes/header.php'; ?>
-    <main>
-        <div class="container-fluid">
-            <h1>À propos de moi</h1>
-            <p>Page en construction...</p>
-            <p>Ici, vous pourrez parler de votre parcours et de vos compétences.</p>
-        </div>
-    </main>
+<main>
+    <div class="container-fluid">
+        <h1>À propos de moi</h1>
+        <p>Page en construction...</p>
+        <p>Ici, vous pourrez parler de votre parcours et de vos compétences.</p>
+    </div>
+</main>
 <?php include 'includes/footer.php'; ?>
--- contact.php
+++ contact.php
@@ -1,9 +1,9 @@
 <?php include 'includes/header.php'; ?>
-    <main>
-        <div class="container-fluid">
-            <h1>Contact</h1>
-            <p>N'hésitez pas à me laisser un message.</p>
-            <!-- Nous ajouterons le formulaire ici plus tard -->
-        </div>
-    </main>
+<main>
+    <div class="container-fluid">
+        <h1>Contact</h1>
+        <p>N'hésitez pas à me laisser un message.</p>
+        <!-- Nous ajouterons le formulaire ici plus tard -->
+    </div>
+</main>
 <?php include 'includes/footer.php'; ?>
